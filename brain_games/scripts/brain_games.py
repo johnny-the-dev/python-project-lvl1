25528dd01c5b865127873318985b4ec9a8294643
Ivan <vanchez666@gmail.com>
2019-12-08 15:42:57 +0300

    add Makefile

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,7 +1,11 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python3.6
+
+from ..cli import run
 
 def main():
     print('Welcome to the Brain Games!')
+    print()
+    run()
 
 if __name__ == "__main__":
     main()
