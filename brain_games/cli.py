968074704e7b13b7133b2f37817372bc99bba6f9
Ivan <vanchez666@gmail.com>
2019-12-11 23:20:42 +0300

    update scripts

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -1,8 +1,10 @@
 import prompt
 
+
 def get_user_name():
     user_name = prompt.string('May I have your name? ')
     return user_name
+
 
 def run():
     user_name = get_user_name()
--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -4,35 +4,41 @@ import random
 import prompt
 from brain_games import cli
 
+
 def main():
     print('Welcome to the Brain Games!')
     print('Answer "yes" if number even otherwise answer "no".')
     print()
     brain_even()
 
+
 if __name__ == "__main__":
     main()
 
-def iseven(num):
-    return num % 2 == 0
 
 def brain_even():
     user_name = cli.run()
+    ANSWERS = {
+        'yes': 0,
+        'no': 1
+    }
     for idx in range(3):
-        number = random.randint(1, 100)
         isright = False
+        number = random.randint(1, 100)
         print('Question: {}'.format(number))
         answer = prompt.string('Your answer: ')
-        
-        if (iseven(number) and answer == 'yes') or (not iseven(number) and answer == 'no'):
-            print('Correct!')
-            isright = True
-        elif iseven(number) and answer == 'no':
-            print("'no' is wrong answer ;(. Correct answer was 'yes'.")
-            print("Let's try again,", user_name)
-        elif not iseven(number) and answer == 'yes':
-            print("'yes' is wrong answer ;(. Correct answer was 'no'.")
-            print("Let's try again,", user_name)
+        even_check = number % 2
+
+        if answer in ANSWERS.keys():
+            if even_check == ANSWERS[answer]:
+                print('Correct!')
+                isright = True
+            elif answer == 'yes':
+                print("yes is wrong answer ;(. Correct answer was no.")
+                print("Let's try again,", user_name)
+            else:
+                print("no is wrong answer ;(. Correct answer was yes.")
+                print("Let's try again,", user_name)
         else:
             print('Wrong answer!')
 
