34c0e9ade61dff44ae8b1ad631620d65cea57d54
Ivan Arkhipov <vanchez666@gmail.com>
2020-04-29 22:14:54 +0300

    added docstring

--- brain_games/start.py
+++ brain_games/start.py
@@ -6,6 +6,15 @@ ROUND_NUMBER = 3
 
 
 def get_user_name(rule_text):
+    """Greets user, prints rules and finds out user`s name.
+
+    Arguments:
+        rule_text {string} -- rule of the game.
+
+    Returns:
+        string -- user`s name.
+    """
+
     print(f'Welcome to the Brain Games!\n{rule_text}\n')
     user_name = prompt.string('May I have your name? ')
     print(f'Hello, {user_name}!\n')
@@ -13,6 +22,20 @@ def get_user_name(rule_text):
 
 
 def check_answer(user_ans, calc_res, ans_pattern):
+    """Games has different types of answers. This function checks user`s
+    answer for matching to the pattern of the game`s answer and then compares
+    user`s answer with the correct result.
+
+    Arguments:
+        user_ans {string} -- user`s answer
+        calc_res {integer or string} -- calculated correct result, can be
+            integer or [yes|no]
+        ans_pattern {string} -- pattern of the game`s answer
+
+    Returns:
+        boolean -- result of checking
+    """
+
     if not re.match(ans_pattern, user_ans):
         print('Wrong answer!')
         return False
@@ -26,6 +49,12 @@ def check_answer(user_ans, calc_res, ans_pattern):
 
 
 def play(game):
+    """Launches a game, checks user`s answer and prints result.
+
+    Arguments:
+        game {module} -- the game module
+    """
+
     user_name = get_user_name(game.RULE_TEXT)
     isSuccess = True
 
